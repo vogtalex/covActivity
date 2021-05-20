2874f210068fbd088a896d2906e0055dc78db1e8
Alex Vogt <vogtalex@oregonstate.edu>
2021-05-20 16:27:29 -0700

    Fixed leap year unittests

--- testLeapYear.py
+++ testLeapYear.py
@@ -4,9 +4,9 @@ import leapYear
 class testCaseAdd(unittest.TestCase):
 	def test_add(self):
 		self.assertEqual(leapYear.leapYearFinder(4), True)
-		self.assertEqual(leapYear.leapYearFinder(100), True)
+		self.assertEqual(leapYear.leapYearFinder(100), False)
 		self.assertEqual(leapYear.leapYearFinder(400), True)
-		self.assertEqual(leapYear.leapYearFinder(3), True)
+		self.assertEqual(leapYear.leapYearFinder(3), False)
 
 if __name__ == '__main__':
 	unittest.main()
